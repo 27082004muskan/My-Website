9588b9aa23d6b6ddfd96272b07fbf0e555729bd3
Muskan Gupta <mksgupta.2004@gmail.com>
2025-08-14 15:21:52 +0530

    Fix import paths for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from'react';
-import Navbar from "../../MyWebsite/src/components/Navbar";
-import Hero from "../../MyWebsite/src/components/Hero";
-import About from "../../MyWebsite/src/components/About";
-import Skills from "../../MyWebsite/src/components/Skills";
-import Projects from "../../MyWebsite/src/components/Projects";
-import Contact from "../../MyWebsite/src/components/Contact";
-import Footer from "../../MyWebsite/src/components/Footer";
-import Certifications from "../../MyWebsite/src/components/Certifications";
-import Activities from "../../MyWebsite/src/components/Activities";
-import Experience from "../../MyWebsite/src/components/Experience";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Certifications from "./components/Certifications";
+import Activities from "./components/Activities";
+import Experience from "./components/Experience";
 import './App.css'
-import Quote from "../../MyWebsite/src/components/Quote";
+import Quote from "./components/Quote";
 
 
 function App() {
